36be51131379a87b32eaa4f301bdaf145120c335
Michael Low <mike@lows.sg>
2025-04-02 02:08:52 +0800

    create server to send map over websockets

--- ros/labs/SlamLab/nodes/websocket_node.py
+++ ros/labs/SlamLab/nodes/websocket_node.py
@@ -9,38 +9,63 @@ from pubsub.pub_sub_manager import ManagedPubSubRunnable, PubSubMsg
 from pubsub.pub_sub_manager import publish, subscribe, unsubscribe, getMessages, getCurrentExecutionContext  
 
 # Constants
-WEBSOCKET_RECV_TOPIC = "websocket/recv"
 ARDUINO_SEND_TOPIC = "arduino/send"
+SLAM_MAPPOSE_TOPIC = "slam/mappose"
 
 def websocketThread(setupBarrier:Barrier=None, readyBarrier:Barrier=None):
     ctx:ManagedPubSubRunnable = getCurrentExecutionContext()
 
     # Perform any setup here
     setupBarrier.wait() if readyBarrier != None else None
-
     print("starting websocket server")
+    subscribe(topic=SLAM_MAPPOSE_TOPIC, ensureReply=True, replyTimeout=1)
 
     # Wait for all Threads ready
     readyBarrier.wait() if readyBarrier != None else None
     asyncio.run(server(ctx))
     
     ctx.doExit()
-    print("Exiting Websocket Recv Thread")
+    print("Exiting Websocket Thread")
 
 async def server(ctx):
-    recv_server_task = asyncio.create_task(start_recv_server())
-    
+    recv_server_task = asyncio.create_task(start_command_server())
+    map_server_task = asyncio.create_task(start_map_server())
     while not ctx.isExit():
         await asyncio.sleep(1)
     recv_server_task.cancel()
+    map_server_task.cancel()
 
-async def recvCommands(websocket):
+async def start_command_server():
+    server = await serve(recv_commands, "", 8765)
+    await server.serve_forever()
+
+async def start_map_server():
+    server = await serve(send_map_data, "", 8766)
+    await server.serve_forever()
+
+async def send_map_data(websocket):
+    print("Sending websocket connection established.")
+    try:
+        while True:
+            messages = getMessages()
+            if(messages):
+                x, y, theta, mapbytes = PubSubMsg.getPayload(messages[-1])
+                await websocket.send(str(x))
+                await websocket.send(str(y))
+                await websocket.send(str(theta))
+                await websocket.send(mapbytes)
+                await asyncio.sleep(0.5)
+    except websockets.exceptions.ConnectionClosedOK:
+        print("Sending websocket connection closed.")
+
+async def recv_commands(websocket):
     params = [0] * 16
     params[1] = 100
-    print("Recv websocket connection established.")
+    print("Receiving websocket connection established.")
     try:
         while True:
             message = await websocket.recv()
+            print(f"Command received: {message}")
             match message:
                 case "f":
                     command = TCommandType.COMMAND_FORWARD
@@ -56,12 +81,8 @@ async def recvCommands(websocket):
             publish(ARDUINO_SEND_TOPIC, commandPacket)
             await websocket.send(f"Echo: {message}")
     except websockets.exceptions.ConnectionClosedOK:
-        print("Recv websocket connection Closed.")
+        print("Receiving websocket connection closed.")
     finally:
         commandPacket = (TPacketType.PACKET_TYPE_COMMAND, TCommandType.COMMAND_STOP, params)
         publish(ARDUINO_SEND_TOPIC, commandPacket)
-        print("stopping robot")
-
-async def start_recv_server():
-    server = await serve(recvCommands, "", 8765)
-    await server.serve_forever()
+        print("Stopping robot, websockets connection closed")
